f352ced20351c8acaf667831e09e557a49a0f450
Hardline98 <d.60wey@gmail.com>
2015-06-07 16:21:20 +0100

    The start of something new
    Added a few base files to get the ball rolling

---
+++ web/index.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ChatRoom V2</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="handler.js"></script>
+    <link type="text/css" rel="stylesheet" href="house.css" />
+</head>
+<body>
+
+<div id="header"><span class="label">Username</span><input type="text" name="inputField" id="username" /></div>
+<div class="content">
+</div>
+
+</body>
+</html>
